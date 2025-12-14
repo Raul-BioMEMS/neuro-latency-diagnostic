22c3ad1f2f05b6611dc471b777d9a5cb984b2b10
Weyland-Yutani-Copr <montoyaraul34@gmail.com>
2025-12-14 15:12:30 -0600

    Initialize script.js with project details
    Added initial comments and variable declaration.

---
+++ script.js
@@ -0,0 +1,8 @@
+/**
+ * Project: Neurological Response Latency Tester
+ * Author: Raul Montoya Cardenas
+ * Purpose: A web-based tool to measure visual-motor reaction time...
+ */
+
+// Your code starts here...
+let startTime = 0;
